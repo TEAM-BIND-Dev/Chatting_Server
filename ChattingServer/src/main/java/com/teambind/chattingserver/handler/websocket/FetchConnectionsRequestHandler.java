995b98cd80d5a969aa9c69e8d5bb99015d8f9ca8
ddingjoo <ddingsha9@teambind.co.kr>
2025-11-03 19:17:17 +0900

    [FEAT] 사용자 연결 요청 거절 및 조회 요청 처리 로직 추가
    ChattingServer/src/main/java/com/teambind/chattingserver/handler/websocket/RejectRequestHandler.java: 연결 요청 거절 처리 핸들러 추가
    ChattingServer/src/main/java/com/teambind/chattingserver/handler/websocket/FetchConnectionsRequestHandler.java: 연결 상태 조회 요청 처리 핸들러 추가
    
    참고: 통합 테스트 추가 필요

---
+++ ChattingServer/src/main/java/com/teambind/chattingserver/handler/websocket/FetchConnectionsRequestHandler.java
@@ -0,0 +1,44 @@
+package com.teambind.chattingserver.handler.websocket;
+
+import com.teambind.auth.dto.Connection;
+import com.teambind.auth.dto.User;
+import com.teambind.auth.entity.UserId;
+import com.teambind.chattingserver.dto.websocket.inbound.FetchConnectionsRequest;
+import com.teambind.chattingserver.dto.websocket.inbound.InviteRequest;
+import com.teambind.chattingserver.dto.websocket.outbound.ErrorResponse;
+import com.teambind.chattingserver.dto.websocket.outbound.FetchConnectionsResponse;
+import com.teambind.chattingserver.dto.websocket.outbound.InviteNotification;
+import com.teambind.chattingserver.dto.websocket.outbound.InviteResponse;
+import com.teambind.chattingserver.service.UserConnectionService;
+import com.teambind.chattingserver.session.WebSocketSessionManager;
+import com.teambind.constant.Constants;
+import com.teambind.constant.MessageType;
+import com.teambind.constant.UserConnectionStatus;
+import org.springframework.data.util.Pair;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class FetchConnectionsRequestHandler implements BaseRequestHandler<FetchConnectionsRequest> {
+	
+	private final UserConnectionService userConnectionService;
+	private final WebSocketSessionManager sessionManager;
+	
+	public FetchConnectionsRequestHandler(UserConnectionService userConnectionService, WebSocketSessionManager sessionManager) {
+		this.userConnectionService = userConnectionService;
+		this.sessionManager = sessionManager;
+	}
+	
+	@Override
+	public void handleRequest(WebSocketSession senderSession, FetchConnectionsRequest request) {
+		UserId senderUserId = (UserId) senderSession.getAttributes().get(Constants.USER_ID.getValue());
+		List<Connection> connections = userConnectionService.getUsersByStatus(senderUserId, request.getStatus()).stream().map(
+				user -> new Connection(user.userName(), request.getStatus())).toList();
+		
+		sessionManager.sendMessage(senderSession, new FetchConnectionsResponse(connections));
+		
+	}
+}
