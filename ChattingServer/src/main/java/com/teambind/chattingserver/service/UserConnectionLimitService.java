f3bbc45f2360f6abdcbaf670e88f3a1b736b865a
ddingjoo <ddingsha9@teambind.co.kr>
2025-11-05 19:05:20 +0900

    ˆ[FEAT] 사용자 연결 끊기(disconnect) 로직 추가
    ChattingServer/src/main/java/com/teambind/chattingserver/service/UserConnectionLimitService.java: 사용자 연결 끊기 로직 추가 및 상태 갱신 처리 구현
    
    참고: 통합 테스트 추가 필요

--- ChattingServer/src/main/java/com/teambind/chattingserver/service/UserConnectionLimitService.java
+++ ChattingServer/src/main/java/com/teambind/chattingserver/service/UserConnectionLimitService.java
@@ -61,4 +61,38 @@ public class UserConnectionLimitService {
 		userRepository.save(firstUserEntity);
 		userRepository.save(secondUserEntity);
 	}
+	
+	
+	@Transactional
+	public void disconnect(UserId senderUserId, UserId partnerUserId) {
+		Long firstUserId = Long.min(senderUserId.id(), partnerUserId.id());
+		Long secondUserId = Long.max(senderUserId.id(), partnerUserId.id()); // 교착상태 대드락 때문에 이렇게 코딩
+		
+		
+		UserEntity firstUserEntity = userRepository.findForUpdateByUserId(firstUserId).orElseThrow(() -> new EntityNotFoundException("Invalid user id: " + firstUserId));
+		UserEntity secondUserEntity = userRepository.findForUpdateByUserId(secondUserId).orElseThrow(() -> new EntityNotFoundException("Invalid user id: " + secondUserId));
+		
+		UserConnectionEntity userConnectionEntity = userConnectionRepository
+				.findByPartnerUserAIdAndPartnerUserBIdAndStatus(firstUserId, secondUserId, UserConnectionStatus.ACCEPTED).orElseThrow(() -> new EntityNotFoundException("Invalid user connection"));
+		
+		Function<Long, String> getErrorMessage = userId -> userId.equals(senderUserId.id())
+				? "Connection limit reached." : "Connection limit reached by the other user.";
+		int firstConnectionCount = firstUserEntity.getConnectionCount();
+		if (firstConnectionCount <= 0) {
+			throw new IllegalStateException("Count is already zero. userId = " + firstUserId);
+		}
+		
+		int secondConnectionCount = secondUserEntity.getConnectionCount();
+		if (secondConnectionCount <= 0) {
+			throw new IllegalStateException("Count is already zero. userId = " + secondUserId);
+		}
+		
+		firstUserEntity.setConnectionCount(firstConnectionCount -1);
+		secondUserEntity.setConnectionCount(secondConnectionCount -1);
+		userConnectionEntity.setStatus(UserConnectionStatus.DISCONNECTED);
+		
+		userConnectionRepository.save(userConnectionEntity);
+		userRepository.save(firstUserEntity);
+		userRepository.save(secondUserEntity);
+	}
 }
